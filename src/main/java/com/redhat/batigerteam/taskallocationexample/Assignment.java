687dfa1e0ce925781133f1207a86419fe5800af7
pamAdmin <>
2019-10-30 15:14:52 -0400

    {/src/main/java/com/redhat/batigerteam/taskallocationexample/Assignment.java}

--- src/main/java/com/redhat/batigerteam/taskallocationexample/Assignment.java
+++ src/main/java/com/redhat/batigerteam/taskallocationexample/Assignment.java
@@ -36,7 +36,7 @@ public class Assignment implements java.io.Serializable {
 	}
 	
 	@Override
-	public toString() {
+	public String toString() {
 	    return "\n{\"actor\": \""+actor+"\", \"role:\""+role+"\"}\n";
 	}
 
